e7b828f5708d913439e8dd20ba8be0b1b5e80e88
Lei Zhang <zhanglei@apache.org>
2021-05-08 23:30:22 +0800

    Add License header

--- src/main/java/com/github/coolbeevip/exports/ExportDependencyToCsv.java
+++ src/main/java/com/github/coolbeevip/exports/ExportDependencyToCsv.java
@@ -1,6 +1,22 @@
-package com.github.springbees.exports;
+/**
+ * Copyright © 2020 Lei Zhang (zhanglei@apache.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import com.github.springbees.pojo.DependencyEntry;
+package com.github.coolbeevip.exports;
+
+import com.github.coolbeevip.pojo.DependencyEntry;
 import java.util.List;
 import java.util.stream.Collectors;
 
--- src/test/java/com/github/coolbeevip/DependencyLicenseListMojoTest.java
+++ src/test/java/com/github/coolbeevip/DependencyLicenseListMojoTest.java
@@ -1,11 +1,27 @@
-package com.github.springbees;
+/**
+ * Copyright © 2020 Lei Zhang (zhanglei@apache.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.coolbeevip;
 
 import static org.junit.Assert.assertEquals;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.github.springbees.parse.ParseMavenCentralRepositorySearch;
-import com.github.springbees.pojo.DependencyEntry;
-import com.github.springbees.storage.MavenRepositoryStorage;
+import com.github.coolbeevip.parse.ParseMavenCentralRepositorySearch;
+import com.github.coolbeevip.pojo.DependencyEntry;
+import com.github.coolbeevip.storage.MavenRepositoryStorage;
 import java.util.Optional;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
